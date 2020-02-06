714a057fe5869c488e58ca5429c2d0a0a9ba544e
jorcelinojunior <jorcelino@live.com>
2020-02-06 08:45:14 -0300

    Finalizado o básico de Angular
    Finalizado o básico de Angular

---
+++ LojaVirtual.Front/LojaVirtual.Web/ClientApp/src/app/produto/produto.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-produto',
+  template: '<html><body> {{ obterNome() }} </body></html>'
+})
+
+export class ProdutoComponent { // Padrão adotado por convenção de PascalCase
+  public nome: string;
+  public liberadoParaVenda: boolean; // Padrão adotado por convenção de camelCase
+
+  public obterNome(): string {
+    // return this.nome;
+    return 'Samsung';
+  }
+}
