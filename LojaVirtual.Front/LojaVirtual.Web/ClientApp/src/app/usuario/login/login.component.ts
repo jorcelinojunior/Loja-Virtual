e5831085552b007f270e30bb680254f3835b1880
jorcelinojunior <jorcelino@live.com>
2020-02-12 17:32:58 -0300

    Melhoria na validação do formulário usando classes do bootstrap e ngClass
    Melhoria na validação do formulário usando classes do bootstrap e ngClass.

--- LojaVirtual.Front/LojaVirtual.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ LojaVirtual.Front/LojaVirtual.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -10,18 +10,11 @@ import { Usuario } from 'src/app/Model/usuario';
 export class LoginComponent {
   public usuario;
   public usuarioAutenticado: boolean;
-  public usuarios = ['usuario1', 'usuario2', 'usuario3', 'usuario4', 'usuario5'];
 
   constructor() {
     this.usuario            = new Usuario();
     this.usuarioAutenticado = false;
   }
-
-  public email       = '';
-  public senha       = '';
-  public logoEmpresa = './../../../assets/images/logo-empresa.png';
-  public title       = 'Logo Padrão da Empresa';
-
 
   entrar(): void {
     if (this.usuario.email === 'jorcelino@live.com' && this.usuario.senha === '123'){
