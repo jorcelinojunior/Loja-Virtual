455e83b13b5e9f44a2fa38181eedbca8246e49a1
jorcelinojunior <jorcelino@live.com>
2020-02-12 15:32:56 -0300

    Agregando detalhes no component Login
    Agregando detalhes no component Login

--- LojaVirtual.Front/LojaVirtual.Web/ClientApp/src/app/usuario/login/login.component.ts
+++ LojaVirtual.Front/LojaVirtual.Web/ClientApp/src/app/usuario/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Usuario } from 'src/app/Model/usuario';
 
 @Component({
   selector: 'app-login',
@@ -7,5 +8,32 @@ import { Component } from '@angular/core';
 })
 
 export class LoginComponent {
+  public usuario;
+  public usuarioAutenticado: boolean;
+  public usuarios = ['usuario1', 'usuario2', 'usuario3', 'usuario4', 'usuario5'];
+
+  constructor() {
+    this.usuario            = new Usuario();
+    this.usuarioAutenticado = false;
+  }
+
+  public email       = '';
+  public senha       = '';
+  public logoEmpresa = './../../../assets/images/logo-empresa.png';
+  public title       = 'Logo Padrão da Empresa';
+
+
+  entrar(): void {
+    if (this.usuario.email === 'jorcelino@live.com' && this.usuario.senha === '123'){
+      this.usuarioAutenticado = true;
+    }
+    console.log('email: ' + this.usuario.email + ' - senha:' + this.usuario.senha);
+  }
+
+  on_keypress(event: any): void {
+    console.log('email: ' + this.usuario.email + ' - senha:' + this.usuario.senha);
+  }
+
+
 
 }
